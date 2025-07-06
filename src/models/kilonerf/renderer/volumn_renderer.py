fed2435e30da6dff4962970c8fdd7f3918078786
XinYuan Li <1501454486@qq.com>
2025-07-06 10:46:01 +0800

    refactor(sampler): remove sampler now

--- src/models/kilonerf/renderer/volumn_renderer.py
+++ src/models/kilonerf/renderer/volumn_renderer.py
@@ -9,12 +9,33 @@ import nerfacc
 class VolumnRenderer(nn.Module):
     def __init__(self, net):
         super().__init__()
-        self.sampler = Sampler()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # self.sampler = Sampler()
         self.net = net
         # self.raw_noise_std = cfg.task_arg.raw_noise_std
         # self.ert_threshold = cfg.sampler.ert_threshold
         self.white_bkgd = cfg.task_arg.white_bkgd
         # self.chunk_size = cfg.task_arg.renderer_chunk_size
+
+        # grid path
+        res_str = f"{self.occ_grid_resolution[0]}_{self.occ_grid_resolution[1]}_{self.occ_grid_resolution[2]}"
+        self.grid_path = os.path.join(self.result_dir, f'occ_grid_res{res_str}_thresh{self.occ_threshold}.pth')
+
+        scene_aabb = torch.tensor(cfg.task_arg.aabb['min'] + cfg.task_arg.aabb['max'], dtype=torch.float32, device=self.device)
+
+        self._load_grid()
+        if self.occ_grid is None:
+            self._evaluate_grid()
+            self._save_grid()
+        
+        self.estimator = nerfacc.OccGridEstimator(
+            roi_aabb = scene_aabb,
+            resolution = self.occ_grid.shape
+            levels = 1
+        ).to(self.device)
+
+        self.estimator.binaries = occ_grid.flatten()
+        self.render_step_size = cfg.sampler.render_step_size
 
 
     def forward(self, batch, is_training: bool = True):
@@ -22,10 +43,6 @@ class VolumnRenderer(nn.Module):
         batch_size, N_rays, _ = rays_o.shape
         rays_o_flat, viewdirs_flat = rays_o.view(-1, 3), viewdirs.view(-1, 3)
         num_total_rays = batch_size * N_rays
-
-        # call sampler to get sampling regions
-        with torch.no_grad():
-            ray_indices, t_start, t_ends = self.sampler(rays_o_flat, viewdirs_flat)
 
         # define callback function
         def rgb_alpha_fn(t_starts, t_ends, ray_indices):
@@ -43,11 +60,12 @@ class VolumnRenderer(nn.Module):
 
         # call rendering function of nerfacc
         colors, opacities, depths, _ = nerfacc.rendering(
-            ray_indices = ray_indices,
-            t_starts = t_starts,
-            t_ends = t_ends,
-            n_rays = total_num_rays,
-            rgb_alpha_fn = rgb_alpha_fn
+            rays_o = rays_o_flat,
+            rays_d = viewdirs_flat,
+            scene_aabb = self.estimator.roi_aabb,
+            estimator = self.estimator,
+            rgb_alpha_fn = rgb_alpha_fn,
+            render_step_size = self.render_step_size
         )
 
         # deal with background
@@ -183,15 +201,124 @@ class VolumnRenderer(nn.Module):
     #     image['alpha_map'] = A_acc.view(batch_size, N_rays, 1)
     #     return image
 
+    # def add_noise(self, raw_density):
+    #     """
+    #     This function is responsible for adding noise to the raw density values.
 
-    def add_noise(self, raw_density):
+    #     @param raw_density: The raw density values.
+    #     @return: The raw density values with added noise.
+
+    #     """
+    #     noise = torch.randn(raw_density.shape, device = raw_density.device) * self.raw_noise_std
+    #     raw_density = raw_density + noise
+    #     return raw_density
+
+
+    def _save_grid(self):
         """
-        This function is responsible for adding noise to the raw density values.
-
-        @param raw_density: The raw density values.
-        @return: The raw density values with added noise.
-
+        Save grid to the disk
         """
-        noise = torch.randn(raw_density.shape, device = raw_density.device) * self.raw_noise_std
-        raw_density = raw_density + noise
-        return raw_density
+        os.makedirs(self.result_dir, exist_ok = True)
+        torch.save(self.occ_grid, self.grid_path)
+        print(f"Occupancy grid saved to {self.grid_path}")
+
+    
+    def _load_grid(self):
+        """
+        Load grid from disk
+        """
+        self.occ_grid = torch.load(self.grid_path, map_location = self.device)
+        print(f"Occupancy grid loaded from {self.grid_path}")
+
+
+    @torch.no_grad()
+    def _evaluate_grid(self, verbose: bool = False):
+        """
+        Use a teacher model (NeRF) to evaluate and generate grid.
+        This process will traverse all the grids, and in each grid, it will generate sub_grid^3 to evaluate whether this grid is occupird or not.
+        """
+        teacher_model = make_network(cfg).cuda()
+        load_network(teacher_model, cfg.teacher_model_dir)
+        if verbose is True:
+            print("teacher model: ", teacher_model)
+        # initialize a bool grid with all False
+        self.occ_grid = torch.zeros(self.occ_grid_resolution, dtype = torch.bool, device = self.device)
+
+        # calculate size of every grid from aabb and resolution
+        cell_size = (self.aabb_max - self.aabb_min) / self.occ_grid_resolution_tensor
+
+        # calculate central coordinates of every cell
+        x = torch.linspace(self.aabb_min[0], self.aabb_max[0], self.occ_grid_resolution[0] + 1, device = self.device)[:-1]
+        y = torch.linspace(self.aabb_min[1], self.aabb_max[1], self.occ_grid_resolution[1] + 1, device = self.device)[:-1]
+        z = torch.linspace(self.aabb_min[2], self.aabb_max[2], self.occ_grid_resolution[2] + 1, device = self.device)[:-1]
+        grid_x, grid_y, grid_z = torch.meshgrid(x, y, z, indexing = 'ij')
+        cell_centers = torch.stack([grid_x, grid_y, grid_z], dim = -1).to(self.device)
+        cell_centers += cell_size / 2.0
+
+        if verbose is True:
+            print("shape of cell_centers: ", cell_centers.shape)
+
+        # generate a 3x3x3 subgrid offset
+        offsets = torch.stack(torch.meshgrid([
+            torch.tensor([-0.25, 0, 0.25]),
+            torch.tensor([-0.25, 0, 0.25]),
+            torch.tensor([-0.25, 0, 0.25])
+        ], indexing='ij'), dim=-1).view(-1, 3).to(self.device)
+        # scale offsets to cell_size
+        scaled_offsets = offsets * cell_size
+
+        # flatten centers of all cells in order to batch
+        flat_cell_centers = cell_centers.view(-1, 3)
+
+        indices = torch.stack(torch.meshgrid(
+            torch.arange(self.occ_grid_resolution[0], device = self.device),
+            torch.arange(self.occ_grid_resolution[1], device = self.device),
+            torch.arange(self.occ_grid_resolution[2], device = self.device),
+            indexing='ij'
+        ), dim=-1).view(-1, 3)
+
+        for i in tqdm(range(0, flat_cell_centers.shape[0], self.chunk), desc="Evaluating Occupancy Grid"):
+            # get centers of current block
+            current_centers = flat_cell_centers[i:i + self.chunk]
+
+            # generate 27 subsamples for current block
+            # shape: (chunk_size, 27, 3)
+            sample_points = current_centers.unsqueeze(1) + scaled_offsets.unsqueeze(0)
+
+            # shape: (chunk_size * 27, 3)
+            flat_sample_points = sample_points.view(-1, 3)
+
+            # reshape to (1, N, 3)
+            inputs = flat_sample_points.unsqueeze(0)
+
+            # use teacher model to evaluate densities
+            # densities is not related to viewdirs, so we use dummy viewdirs instead
+            dummy_viewdirs = torch.zeros(1, 3, device = self.device)
+
+            # shape: (1, N, 4)
+            raw_output = teacher_model(inputs, dummy_viewdirs, "fine")
+
+            if verbose is True:
+                print("shape of raw_output: ", raw_output.shape)
+
+            densities = raw_output.view(-1, 4)[..., -1]
+
+            if verbose is True:
+                print("densities of teacher model: ", densities)
+
+            chunk_densities = densities.view(-1, 27)
+            
+            # if any densities of a subgrid > threshold, we consider this grid to be occupied
+            occupied_mask = torch.any(chunk_densities > self.occ_threshold, dim = -1)
+
+            # write results back to grids
+            # get current indexes of the original grids
+            current_indices = indices[i:i + self.chunk]
+
+            occupied_indices = current_indices[occupied_mask]
+
+            self.occ_grid[occupied_indices[:, 0], occupied_indices[:, 1], occupied_indices[:, 2]] = True
+
+            if verbose is True:
+                occupied_ratio = self.occ_grid.sum() / self.occ_grid.numel()
+                print(f"Occupied ratio: {occupied_ratio:.4f}")
